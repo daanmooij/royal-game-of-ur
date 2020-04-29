634e53c8a324acf38c0a40ef063d2d87ec1d8810
Daan Mooij <daanmooij@gmail.com>
2020-04-29 20:35:47 +0200

    init repo

---
+++ tests/unit/PlayerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DaanMooij\RoyalGameOfUr;
+
+use PHPUnit\Framework\TestCase;
+
+class PlayerTest extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
